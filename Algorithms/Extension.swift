8ec15bb8df12076f30f63ca94933598c9508efda
Denis <danis.danilyuk@gmail.com>
2020-04-15 16:43:19 +0300

    Revert "add Lab3, add Charts"
    This reverts commit d3521e9cd8d8c02af70709f720d583c3ed8492e2.

--- Algorithms/Extension.swift
+++ Algorithms/Extension.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-
 extension UIView {
     class var identifier: String {
         return String(describing: self)
     }
 }
-
 
 extension UIViewController {
     
@@ -22,7 +20,6 @@ extension UIViewController {
         return String(describing: self)
     }
 }
-
 
 extension UIViewController {
     func hideKeyboard() {
@@ -33,38 +30,5 @@ extension UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
-    }
-}
-
-
-@IBDesignable extension UIButton {
-    
-    @IBInspectable var borderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
-        get {
-            return layer.borderWidth
-        }
-    }
-    
-    @IBInspectable var cornerRadius: CGFloat {
-        set {
-            layer.cornerRadius = newValue
-        }
-        get {
-            return layer.cornerRadius
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        set {
-            guard let uiColor = newValue else { return }
-            layer.borderColor = uiColor.cgColor
-        }
-        get {
-            guard let color = layer.borderColor else { return nil }
-            return UIColor(cgColor: color)
-        }
     }
 }
--- Algorithms/TaskViewController.swift
+++ Algorithms/TaskViewController.swift
@@ -18,48 +18,49 @@ class TaskViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        // Do any additional setup after loading the view.
     }
-}
-
-
-extension TaskViewController: UITableViewDataSource, UITableViewDelegate {
     
+
+    
+
+}
+extension TaskViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if labName == "1" {
             return 3
-        } else {
+        } else if labName == "2" {
             return 1
+        } else {
+            return 3
         }
     }
     
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "reuseID")
-
         if labName == "1" {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "reuseID")
             let taskNames = ["Завдання 1", "Завдання 2", "Завдання 3"]
             cell.textLabel?.text = taskNames[indexPath.row]
             
             cell.accessoryType = .disclosureIndicator
             return cell
         } else if labName == "2" {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "reuseID")
             let taskNames = ["Порозрядне сортування"]
             cell.textLabel?.text = taskNames[indexPath.row]
             return cell
-        } else if labName == "3" {
-            let taskNames = ["Схема Ейткена"]
-            cell.textLabel?.text = taskNames[indexPath.row]
-            return cell
-        } else if labName == "4" {
-            let taskNames = ["Метод половинного ділення"]
-            cell.textLabel?.text = taskNames[indexPath.row]
-            return cell
         } else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "reuseID")
+            let taskNames = ["Завдання 1", "Завдання 2", "Завдання 3", "Завдання 4"]
+            cell.textLabel?.text = taskNames[indexPath.row]
             return cell
         }
     }
     
-        
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let mainStoryBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
 
@@ -73,42 +74,22 @@ extension TaskViewController: UITableViewDataSource, UITableViewDelegate {
             } else if indexPath.row == 2{
                 guard let vc = mainStoryBoard.instantiateViewController(identifier: Lab1Task3ViewController.identifier) as? Lab1Task3ViewController else { return }
                 self.navigationController?.pushViewController(vc, animated: true)
+                
             }
+            
+            
         } else if labName == "2" {
-            if indexPath.row == 0 {
-                guard let vc = mainStoryBoard.instantiateViewController(identifier: Lab2Task1ViewController.identifier) as? Lab2Task1ViewController else { return }
-                self.navigationController?.pushViewController(vc, animated: true)
-            }
-        } else if labName == "3" {
-           if indexPath.row == 0 {
-               guard let vc = mainStoryBoard.instantiateViewController(identifier: Lab3MainViewController.identifier) as? Lab3MainViewController else { return }
-               self.navigationController?.pushViewController(vc, animated: true)
-            }
-        } else if labName == "4" {
-           if indexPath.row == 0 {
-               guard let vc = mainStoryBoard.instantiateViewController(identifier: Lab4MainViewController.identifier) as? Lab4MainViewController else { return }
-               self.navigationController?.pushViewController(vc, animated: true)
-            }
+                if indexPath.row == 0 {
+                    guard let vc = mainStoryBoard.instantiateViewController(identifier: Lab2Task1ViewController.identifier) as? Lab2Task1ViewController else { return }
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+        
+            tableView.deselectRow(at: indexPath, animated: true)
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = .clear
-        
-        return view
-    }
-    
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 0.001
-    }
 }
